162603e276f9cde225b516fad3fecb3e3308b67c
Dennis Terhorst <d.terhorst@fz-juelich.de>
2017-01-20 15:52:23 +0100

    added python-task solution hint

--- session3_RegularExpressions/python-task/getdata.py
+++ session3_RegularExpressions/python-task/getdata.py
@@ -3,6 +3,12 @@
 
 import re
 
+kv = re.compile(r'([a-zA-Z _]+) *:  *([0-9.]+)[ a-zA-Z]*')
+
 with open("15270/hpc_benchmark-S1-N2.2222-P1-T168.out", 'r') as infile:
+    d = dict()
     for line in infile:
-        print(line.strip('\n'))
+        for match in kv.findall(line):
+            d[match[0].strip()] = float(match[1])
+
+print(d)
